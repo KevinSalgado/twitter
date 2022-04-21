2b22e962f12eb7286ad9f0c88a4f37716d34e0ff
Kevin_Salgado <sgkevinsg@gmail.com>
2022-04-21 17:47:31 -0500

    re2

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -2,20 +2,13 @@ const user = require("./../../app/models/user");
 
 describe("Unit Tests for User class", () => {
   test("Create an Iser object", () => {
-    const user1 = new user(
-      1,
-      "carlogilmar",
-      "Carlo",
-      "Bio",
-      "dateCreated",
-      "lastUpdated"
-    );
+    const user1 = new user(1, "carlogilmar", "Carlo", "Bio");
 
     expect(user1.id).toBe(1);
     expect(user1.username).toBe("carlogilmar");
     expect(user1.name).toBe("Carlo");
     expect(user1.bio).toBe("Bio");
-    expect(user1.dateCreated).toBe("dateCreated");
-    expect(user1.lastUpdated).toBe("lastUpdated");
+    expect(user1.dateCreated).not.toBeUndefined();
+    expect(user1.lastUpdated).not.toBeUndefined();
   });
 });
